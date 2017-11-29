7d5c26c5f26c5fe45b01fc69b70c10f67dd79ed2
DUNHAOZHONG <dunhaozhong@gmail.com>
2017-11-29 12:35:05 -0500

    Add leader agent

---
+++ src/main/java/CommonBean/Agent.java
@@ -0,0 +1,13 @@
+package CommonBean;
+
+import jbotsim.Node;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public abstract class Agent{
+
+    protected boolean activate = true;
+    protected AgentTypeEnum type;
+
+}
---
+++ src/main/java/CommonBean/LeaderAgent.java
@@ -0,0 +1,36 @@
+package CommonBean;
+
+import jbotsim.Node;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class LeaderAgent extends Agent {
+
+    private HashMap<Integer,Node> exploredMap = new HashMap<Integer, Node>();
+    private HashMap<Integer,HashMap<Integer, Node> > hop2Neighbours = new HashMap<Integer, HashMap<Integer, Node>>();
+
+    public LeaderAgent(AgentTypeEnum type){
+        this.type = type;
+    }
+
+    public boolean addNewNode(Node node){
+        if(exploredMap.containsKey(node.getID())){
+            System.out.println(node.getID() + "has already explored.");
+        }else{
+            exploredMap.put(node.getID(),node);
+            return true;
+        }
+        return false;
+    }
+
+    public void addHop2Neighbours(Integer id, ArrayList<Node> nodes){
+        if(!hop2Neighbours.containsKey(id)){
+            HashMap<Integer, Node> neigs = new HashMap<Integer, Node>();
+            for(Node node:nodes){
+                neigs.put(node.getID(),node);
+            }
+            hop2Neighbours.put(id, neigs);
+        }
+    }
+}
