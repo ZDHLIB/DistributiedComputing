4629d323fe22b9d637e97bfb86f20659259878ba
DUNHAOZHONG <dunhaozhong@gmail.com>
2017-11-29 12:14:44 -0500

    Add enum of agent type

---
+++ src/main/java/CommonBean/AgentTypeEnum.java
@@ -0,0 +1,25 @@
+package CommonBean;
+
+public enum AgentTypeEnum {
+    BLACKVIRUS("BLACKVIRUS",1),
+    CLONESVIRUS("CLONESVIRUS",2),
+    LEADER("LEADER",3),
+    EXPLORER("EXPLORER",4),
+    CLEANER("CLEANER",5);
+
+    private String name;
+    private int index;
+
+    AgentTypeEnum(String name, int index){
+        this.name = name;
+        this.index = index;
+    }
+
+    public String getName(){
+        return this.name;
+    }
+
+    public int getIndex(){
+        return this.index;
+    }
+}
