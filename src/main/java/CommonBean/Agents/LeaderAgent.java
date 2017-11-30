b5b23a7fd2da66a024a69d21b3e3d35b5df521f2
DUNHAOZHONG <dunhaozhong@gmail.com>
2017-11-30 13:58:10 -0500

    update members of leaderagent, add log

--- src/main/java/CommonBean/Agents/LeaderAgent.java
+++ src/main/java/CommonBean/Agents/LeaderAgent.java
@@ -1,22 +1,43 @@
 package CommonBean.Agents;
 
+import BlackVirusFinding.Initiator;
 import jbotsim.Node;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.ArrayList;
 import java.util.HashMap;
 
 public class LeaderAgent extends Agent {
 
+    private final static Logger logger = LoggerFactory.getLogger(LeaderAgent.class);
+
+    private static LeaderAgent leaderAgent = null;
+    private ExporerAgent exporerAgent = null;
     private HashMap<Integer,Node> exploredMap = new HashMap<Integer, Node>();
+
+    //This is the frontiers, if a node is explored, it should be moved
     private HashMap<Integer,HashMap<Integer, Node> > hop2Neighbours = new HashMap<Integer, HashMap<Integer, Node>>();
 
-    public LeaderAgent(AgentTypeEnum type){
+    private LeaderAgent(AgentTypeEnum type){
         this.type = type;
+    }
+
+    public LeaderAgent getInstance(){
+        if(leaderAgent == null){
+            synchronized (LeaderAgent.class){
+                if(leaderAgent == null){
+                    leaderAgent = new LeaderAgent(AgentTypeEnum.LEADER);
+                    return leaderAgent;
+                }
+            }
+        }
+        return leaderAgent;
     }
 
     public boolean addNewNode(Node node){
         if(exploredMap.containsKey(node.getID())){
-            System.out.println(node.getID() + "has already explored.");
+            logger.info("{} has already explored.", node.getID());
         }else{
             exploredMap.put(node.getID(),node);
             return true;
