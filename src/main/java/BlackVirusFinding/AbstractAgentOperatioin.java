00c202513a5f117240c3fddf5eaead4b64b2b24b
DUNHAOZHONG <dunhaozhong@gmail.com>
2017-11-30 17:51:15 -0500

    Add function of finding Node by id

--- src/main/java/BlackVirusFinding/AbstractAgentOperatioin.java
+++ src/main/java/BlackVirusFinding/AbstractAgentOperatioin.java
@@ -4,8 +4,18 @@ import CommonBean.NodeBean.BasicNode;
 import jbotsim.Node;
 
 import java.util.HashMap;
+import java.util.List;
 
 public abstract class AbstractAgentOperatioin {
 
     public abstract BasicNode chooseTarget(HashMap<Integer, Node> frontiersMap);
+
+    public static BasicNode findNodeByID(int id, List<Node> neighbours){
+        for(Node node : neighbours){
+            if( node.getID() == id ){
+                return (BasicNode)node;
+            }
+        }
+        return null;
+    }
 }
