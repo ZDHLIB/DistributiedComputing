f3e0ef619ed73be26af490aacbf04982455548d9
DUNHAOZHONG <dunhaozhong@gmail.com>
2017-11-30 14:06:55 -0500

    Add explored flag, rewrite the onMessage event

--- src/main/java/CommonBean/NodeBean/BasicNode.java
+++ src/main/java/CommonBean/NodeBean/BasicNode.java
@@ -1,6 +1,7 @@
 package CommonBean.NodeBean;
 
-import CommonBean.Agents.Agent;
+import BlackVirusFinding.GreedyBasedFacade;
+import CommonBean.Agents.*;
 import jbotsim.Message;
 import jbotsim.Node;
 import org.slf4j.Logger;
@@ -10,15 +11,15 @@ import java.util.ArrayList;
 import java.util.HashMap;
 
 
+
 public class BasicNode extends Node {
 
     private final static Logger logger = LoggerFactory.getLogger(BasicNode.class);
 
-    private Agent blackVirusAgent;
-    private Agent cloneVirusAgent;
-    private Agent explorerAgent;
-    private Agent leaderAgent;
-    private Agent shadowAgent;
+    private boolean explored = false;
+    private BlackVirusAgent blackVirusAgent = null;
+    private LeaderAgent leaderAgent = null;
+    private ShadowAgent shadowAgent = null;
     private HashMap<Integer,Node > rasideauDegree = new HashMap<Integer, Node>();
 
     @Override
@@ -37,12 +38,27 @@ public class BasicNode extends Node {
     @Override
     public void onMessage(Message message) {
         // processing of a received message
+        Object obj = message.getContent();
+        //String: visited message
+        if( obj instanceof Integer ){
+
+        } else if( obj instanceof ExporerAgent ){
+
+        } else if( obj instanceof BlackVirusAgent){
+
+        } else if( obj instanceof ShadowAgent ){
+
+        }
     }
 
     @Override
     public void onSelection() {
-        // what to do when this node is selected by the user
-
+        //I am a initiator, start the algorithm
+        if(getID() == 0){
+            //TODO
+            GreedyBasedFacade greedyBasedFacade = new GreedyBasedFacade();
+            greedyBasedFacade.start(this);
+        }
     }
 
 
@@ -60,31 +76,15 @@ public class BasicNode extends Node {
         return blackVirusAgent;
     }
 
-    public void setBlackVirusAgent(Agent blackVirusAgent) {
+    public void setBlackVirusAgent(BlackVirusAgent blackVirusAgent) {
         this.blackVirusAgent = blackVirusAgent;
-    }
-
-    public Agent getCloneVirusAgent() {
-        return cloneVirusAgent;
-    }
-
-    public void setCloneVirusAgent(Agent cloneVirusAgent) {
-        this.cloneVirusAgent = cloneVirusAgent;
-    }
-
-    public Agent getExplorerAgent() {
-        return explorerAgent;
-    }
-
-    public void setExplorerAgent(Agent explorerAgent) {
-        this.explorerAgent = explorerAgent;
     }
 
     public Agent getLeaderAgent() {
         return leaderAgent;
     }
 
-    public void setLeaderAgent(Agent leaderAgent) {
+    public void setLeaderAgent(LeaderAgent leaderAgent) {
         this.leaderAgent = leaderAgent;
     }
 
@@ -92,7 +92,15 @@ public class BasicNode extends Node {
         return shadowAgent;
     }
 
-    public void setShadowAgent(Agent shadowAgent) {
+    public void setShadowAgent(ShadowAgent shadowAgent) {
         this.shadowAgent = shadowAgent;
+    }
+
+    public boolean isExplored() {
+        return explored;
+    }
+
+    public void setExplored(boolean explored) {
+        this.explored = explored;
     }
 }
