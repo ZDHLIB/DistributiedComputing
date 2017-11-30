d940943a17246a8cd06b72b087baf11a98276f55
DUNHAOZHONG <dunhaozhong@gmail.com>
2017-11-29 21:16:35 -0500

    Add source in explorer agent

--- src/main/java/CommonBean/Agents/ExporerAgent.java
+++ src/main/java/CommonBean/Agents/ExporerAgent.java
@@ -6,6 +6,7 @@ import java.util.ArrayList;
 
 public class ExporerAgent extends Agent {
 
+    private int source = 0;
     private int target = 0;
     private ArrayList<Integer> targetNeigs = new ArrayList<Integer>();
 
@@ -13,14 +14,6 @@ public class ExporerAgent extends Agent {
         this.type = type;
     }
 
-
-    public int getTarget() {
-        return target;
-    }
-
-    public void setTarget(int target) {
-        this.target = target;
-    }
 
     public ArrayList<Integer> getTargetNeigs() {
         return targetNeigs;
@@ -31,5 +24,21 @@ public class ExporerAgent extends Agent {
         for(Node node:neigs){
             targetNeigs.add(node.getID());
         }
+    }
+
+    public int getSource() {
+        return source;
+    }
+
+    public void setSource(int source) {
+        this.source = source;
+    }
+
+    public int getTarget() {
+        return target;
+    }
+
+    public void setTarget(int target) {
+        this.target = target;
     }
 }
