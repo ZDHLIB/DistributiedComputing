af0d0aef6546ee92b6ee8ec8bdc68a8ab0dae339
DUNHAOZHONG <dunhaozhong@gmail.com>
2017-11-30 17:50:55 -0500

    Finish: receiving visited, receiving explorer

--- src/main/java/CommonBean/NodeBean/BasicNode.java
+++ src/main/java/CommonBean/NodeBean/BasicNode.java
@@ -9,7 +9,7 @@ import org.slf4j.LoggerFactory;
 
 import java.util.ArrayList;
 import java.util.HashMap;
-
+import java.util.List;
 
 
 public class BasicNode extends Node {
@@ -22,9 +22,14 @@ public class BasicNode extends Node {
     private ShadowAgent shadowAgent = null;
     private HashMap<Integer,Node > rasideauDegree = new HashMap<Integer, Node>();
 
+    private GreedyBasedFacade greedyBasedFacade = null;
+
     @Override
     public void onStart() {
         // initialize the node variables
+        if(greedyBasedFacade == null) {
+            greedyBasedFacade = new GreedyBasedFacade();
+        }
         setSize(15);
         System.out.println("Start "+getID());
     }
@@ -41,8 +46,29 @@ public class BasicNode extends Node {
         Object obj = message.getContent();
         //String: visited message
         if( obj instanceof Integer ){
+            greedyBasedFacade.updateResidualDegree(this, obj);
 
-        } else if( obj instanceof ExporerAgent ){
+        }
+        // Receive explorerAgent, check whether I have been visited or not
+        else if( obj instanceof ExporerAgent ){
+            // The explorer I received is what I sent
+            if( explored ){
+                //Update explored map info
+                greedyBasedFacade.updateExploredInfo(obj);
+                //Start next interation
+                greedyBasedFacade.start();
+            }
+            // I haven't been explored, I am a blackVirus, send clones to neighbours
+            else if (blackVirusAgent != null && blackVirusAgent.getActivate()){
+                List<Node> neighbours = getNeighbors();
+                blackVirusAgent.setTargetNeigs(neighbours);
+                blackVirusAgent.setActivate(false);
+                greedyBasedFacade.sendClones2Neigbours(this, neighbours);
+            }
+            // I am a unexplored node and not a black virus, return explorer with my id and neighbours.
+            else {
+                greedyBasedFacade.returnExplorerAgent(this, obj);
+            }
 
         } else if( obj instanceof BlackVirusAgent){
 
@@ -56,8 +82,7 @@ public class BasicNode extends Node {
         //I am a initiator, start the algorithm
         if(getID() == 0){
             //TODO
-            GreedyBasedFacade greedyBasedFacade = new GreedyBasedFacade();
-            greedyBasedFacade.start(this);
+            greedyBasedFacade.start();
         }
     }
 
@@ -72,7 +97,7 @@ public class BasicNode extends Node {
         }
     }
 
-    public Agent getBlackVirusAgent() {
+    public BlackVirusAgent getBlackVirusAgent() {
         return blackVirusAgent;
     }
 
