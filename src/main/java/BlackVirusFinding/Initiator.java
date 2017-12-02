502677c9b3fdfbe48d7bcc4279b22c5e60e2d146
DUNHAOZHONG <dunhaozhong@gmail.com>
2017-12-01 19:44:29 -0500

    Fix some error

--- src/main/java/BlackVirusFinding/Initiator.java
+++ src/main/java/BlackVirusFinding/Initiator.java
@@ -1,5 +1,6 @@
 package BlackVirusFinding;
 
+import CommonBean.Agents.LeaderAgent;
 import CommonBean.NodeBean.BasicNode;
 import CommonBean.NodeBean.NodeBuilder;
 import jbotsim.Node;
@@ -8,6 +9,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.awt.*;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Random;
 
 public class Initiator {
@@ -26,6 +29,7 @@ public class Initiator {
     public static Topology initTopology(int n, int width, int height, boolean start, Class<? extends Node> var1){
         logger.info("Start initial topology...");
 
+        BasicNode node = null;
         Random random = new Random();
         Topology tp = new Topology(width,height,start);
         tp.setDefaultNodeModel(var1);
@@ -39,30 +43,60 @@ public class Initiator {
             NodeBuilder nodeBuilder = new NodeBuilder(BasicNode.class);
             nodeBuilder.buildId(i)
                        .buildWireless(true)
-                       .buildCommunicationRange(width<height?0.5*width:0.5*height)
-                       .buildResideauDegree();
+                       .buildCommunicationRange(width<height?0.5*width:0.5*height);
 
             //Initial leader, explorer, shadow agents at node 0
             //Initial node with black virus
             if(i == 0){
                 nodeBuilder.buildColor(new Color(123, 27, 7));
-                nodeBuilder.buildBlackVirusAgent()
-                           .buildLeaderAgent();
-
+                nodeBuilder.buildLeaderAgent();
+                node = nodeBuilder.build();
             } else if(i == blackVirusNode) {
                 nodeBuilder.buildColor(new Color(0, 0, 0));
                 nodeBuilder.buildBlackVirusAgent();
+                node = nodeBuilder.build();
+            } else {
+                node = nodeBuilder.build();
             }
-            Node node = nodeBuilder.build();
+
 
             int x = random.nextInt(width);
             int y = random.nextInt(height);
             tp.addNode(x,y,node);
         }
 
+        initResidualDegree(tp);
 
         logger.info("Initial topology finished...");
         return tp;
     }
+
+
+    private static void initSink(BasicNode node){
+
+        LeaderAgent.getInstance().addNewNode(node);
+        HashMap<Integer, Node> neigs = new HashMap<Integer, Node>();
+        for(Node n : node.getNeighbors()){
+            neigs.put(n.getID(), n);
+        }
+        LeaderAgent.getInstance().addHop2Neighbours(node, neigs);
+    }
+
+    public static void initResidualDegree(Topology tp){
+        List<Node> nodeList = tp.getNodes();
+
+        for(Node node : nodeList){
+            BasicNode basicNode = (BasicNode) node;
+            basicNode.getRasideauDegree().clear();
+            for( Node n2 : basicNode.getNeighbors()) {
+                basicNode.addRasideauDegree(n2);
+            }
+            if(basicNode.getID() == 0){
+                initSink(basicNode);
+            }
+        }
+    }
+
+
 
 }
