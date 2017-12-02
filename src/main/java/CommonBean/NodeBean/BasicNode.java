62ce9f03ad626eed08a3a3ed7b46b463afafd603
DUNHAOZHONG <dunhaozhong@gmail.com>
2017-12-02 16:19:25 -0500

    clean

--- src/main/java/CommonBean/NodeBean/BasicNode.java
+++ src/main/java/CommonBean/NodeBean/BasicNode.java
@@ -1,16 +1,14 @@
 package CommonBean.NodeBean;
 
+import BlackVirusFinding.AbstractAgentOperatioin;
 import BlackVirusFinding.GreedyBasedFacade;
 import CommonBean.Agents.*;
 import jbotsim.Message;
 import jbotsim.Node;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import sun.security.provider.SHA;
 
 import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
 
 
 public class BasicNode extends Node {
@@ -18,10 +16,10 @@ public class BasicNode extends Node {
     private final static Logger logger = LoggerFactory.getLogger(BasicNode.class);
 
     private boolean explored = false;
-    private BlackVirusAgent blackVirusAgent = BlackVirusAgent.getInstance();
-    private LeaderAgent leaderAgent = LeaderAgent.getInstance();
+    private BlackVirusAgent blackVirusAgent = null;
+    private LeaderAgent leaderAgent = null;
     private ShadowAgent shadowAgent = null;
-    private HashMap<Integer,Node > rasideauDegree = new HashMap<Integer, Node>();
+    private ArrayList<BasicNode> rasidualNodes = new ArrayList<BasicNode>();
 
     private GreedyBasedFacade greedyBasedFacade = null;
 
@@ -32,7 +30,7 @@ public class BasicNode extends Node {
             greedyBasedFacade = new GreedyBasedFacade();
         }
         setSize(15);
-        System.out.println("Start "+getID());
+        setIcon("/imgs/"+getID()+".png");
     }
 
     @Override
@@ -52,97 +50,53 @@ public class BasicNode extends Node {
         Object obj = message.getContent();
         //String: visited message
         if( obj instanceof Integer ){
-            logger.info("{} Receive visited message from {}", getID(), obj);
-            greedyBasedFacade.updateResidualDegree(this, obj);
+            Integer data = (Integer) obj;
+            logger.info("{} receives visited from {}", getID(), data);
+            greedyBasedFacade.dealWithInteger(this, data);
         }
         // Receive explorerAgent, check whether I have been visited or not
         else if( obj instanceof ExporerAgent ){
-            // The explorer I received is what I sent
-            if( explored ){
-                //Update explored map info
-                greedyBasedFacade.updateExploredInfo(obj);
-                //Start next interation
-                greedyBasedFacade.start();
-            }
-            // I haven't been explored, I am a blackVirus, send clones to neighbours
-            else if (blackVirusAgent != null && blackVirusAgent.getActivate()){
-                List<Node> neighbours = getNeighbors();
-                blackVirusAgent.setTargetNeigs(neighbours);
-                blackVirusAgent.setActivate(false);
-                greedyBasedFacade.sendClones2Neigbours(this, neighbours);
-            }
-            // I am a unexplored node and not a black virus, return explorer with my id and neighbours.
-            else {
-                greedyBasedFacade.returnExplorerAgent(this, obj);
-            }
-
+            logger.info("{} receives ExporerAgent from {}", getID(), message.getSender());
+            ExporerAgent exporerAgent = (ExporerAgent) obj;
+            greedyBasedFacade.dealWithExplorerAgent(this, exporerAgent);
         }
         // Receive black virus's clone
         else if( obj instanceof BlackVirusAgent){
-            BlackVirusAgent cloneVirusAgent = (BlackVirusAgent) obj;
-            if( cloneVirusAgent.getType().equals(AgentTypeEnum.CLONESVIRUS) ){
-                // I have been protected by shadowAgent, so deactivate receiving clones
-                if( this.shadowAgent != null ){
-                    cloneVirusAgent.setActivate(false);
-                }
-                // I am a unexplored node, so destroyed by clones
-                else {
-                    this.blackVirusAgent = cloneVirusAgent;
-                }
-            }
-        } else if( obj instanceof ShadowAgent ){
-            // if I have been destroyed by clones and clone is active, then deactivate clones
-            if( this.blackVirusAgent != null
-                    && this.blackVirusAgent.getActivate()
-                    && this.blackVirusAgent.getType().equals(AgentTypeEnum.CLONESVIRUS)){
-                this.blackVirusAgent.setActivate(false);
-            } else {
-                if( this.leaderAgent != null ) {
-                    // leaderAgent will go to next stop
-
-                }
-                this.shadowAgent = (ShadowAgent) obj;
-            }
-        } else if( obj instanceof LeaderAgent ){
-            LeaderAgent leaderAgent = (LeaderAgent) obj;
-            if( this.shadowAgent != null ){
-                // leaderAgent will go to next stop and assign a shadow agent to next stop
-                leaderAgent.addStepAssign();
-                send( leaderAgent.getShadowAgents().get(leaderAgent.getStepAssign()).getTarget(),
-                        leaderAgent.getShadowAgents().get(leaderAgent.getStepAssign()) );
-                send( leaderAgent.getShadowAgents().get(leaderAgent.getStepAssign()).getTarget(),
-                        leaderAgent);
-            }
-            //protection finished, send explorer to target
-            else if( leaderAgent.getShadowAgents().size() == leaderAgent.getStepAssign() ){
-                this.leaderAgent = leaderAgent;
-                leaderAgent.resetStepAssign();
-                ExporerAgent exporerAgent = ExporerAgent.getInstance();
-                exporerAgent.setSource(this);
-                exporerAgent.setTarget(this.leaderAgent.getTarget());
-                send(this.leaderAgent.getTarget(), ExporerAgent.getInstance());
-            }
+            logger.info("{} receives BlackVirusAgent from {}", getID(), message.getSender());
+            BlackVirusAgent blackVirusAgent = (BlackVirusAgent) obj;
+            greedyBasedFacade.dealWithBlackClonesAgent(this, blackVirusAgent);
+        }
+        // Receive shadowAgent
+        else if( obj instanceof ShadowAgent ){
+            logger.info("{} receives ShadowAgent from {}", getID(), message.getSender());
+            ShadowAgent shadowAgent = (ShadowAgent) obj;
+            greedyBasedFacade.dealWithShadowAgent(this, shadowAgent);
+        }
+        // Receive leaderAgent
+        else if( obj instanceof LeaderAgent ){
+            logger.info("{} receives LeaderAgent from {}", getID(), message.getSender());
+            this.setLeaderAgent(LeaderAgent.getInstance());
+            greedyBasedFacade.dealWithLeaderAgent(this);
         }
     }
 
     @Override
     public void onSelection() {
-        //I am a initiator, start the algorithm
+        //I am a initiator, startExplorer the algorithm
         if(getID() == 0){
             //TODO
-            greedyBasedFacade.start();
+            greedyBasedFacade.startExplorer();
+            AbstractAgentOperatioin.sendVisited2Neighbours(this, this.getNeighbors());
         }
     }
 
 
-    public HashMap<Integer, Node> getRasideauDegree() {
-        return rasideauDegree;
+    public ArrayList<BasicNode> getRasidualNodes() {
+        return rasidualNodes;
     }
 
     public void addRasideauDegree(Node node) {
-        if(!this.rasideauDegree.containsKey(node.getID())) {
-            this.rasideauDegree.put(node.getID(), node);
-        }
+        this.rasidualNodes.add((BasicNode) node);
     }
 
     public BlackVirusAgent getBlackVirusAgent() {
@@ -161,7 +115,7 @@ public class BasicNode extends Node {
         this.leaderAgent = leaderAgent;
     }
 
-    public Agent getShadowAgent() {
+    public ShadowAgent getShadowAgent() {
         return shadowAgent;
     }
 
