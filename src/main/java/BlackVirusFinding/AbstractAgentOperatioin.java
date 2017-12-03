15d03f3c26075e7ca99080dfc7b86066ac421315
DUNHAOZHONG <dunhaozhong@gmail.com>
2017-12-02 22:27:26 -0500

    add find nearest path

--- src/main/java/BlackVirusFinding/AbstractAgentOperatioin.java
+++ src/main/java/BlackVirusFinding/AbstractAgentOperatioin.java
@@ -1,15 +1,24 @@
 package BlackVirusFinding;
 
+import CommonBean.Agents.BlackVirusAgent;
+import CommonBean.Agents.ExporerAgent;
 import CommonBean.Agents.LeaderAgent;
+import CommonBean.Agents.ShadowAgent;
+import CommonBean.MyMessage;
 import CommonBean.NodeBean.BasicNode;
 import jbotsim.Node;
+import jbotsim.Topology;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
+import java.awt.geom.QuadCurve2D;
+import java.util.*;
 
 public abstract class AbstractAgentOperatioin {
-    protected LeaderAgent leaderAgent = LeaderAgent.getInstance();
+    private final static Logger logger = LoggerFactory.getLogger(AbstractAgentOperatioin.class);
+
+
+    protected static LeaderAgent leaderAgent = LeaderAgent.getInstance();
 
     /**
      * get source node and target node
@@ -32,7 +41,85 @@ public abstract class AbstractAgentOperatioin {
     public static void sendVisited2Neighbours(BasicNode node, List<Node> neighbours){
         for(Node n : neighbours){
             BasicNode bn = (BasicNode) n;
-            node.send(bn, node.getID());
+            MyMessage mm = new MyMessage(node.getID(), null);
+            node.send(bn, mm);
+        }
+    }
+
+    /**
+     * Use Floyd to get nearest path
+     * @param source
+     * @param dest
+     * @return
+     */
+    public static Queue<BasicNode> findNearestPath(int source, int dest){
+        Queue<BasicNode> route = new LinkedList<BasicNode>();
+        ArrayList<BasicNode> map = leaderAgent.getExploredMap();
+        Topology tp = leaderAgent.getTp();
+        int size = tp.getNodes().size();
+        int[][] mat = new int[size][size];
+        BasicNode[][] path = new BasicNode[size][size];
+
+        for(int i = 0; i < size; i++){
+            for(int j = 0; j < size; j++){
+                mat[i][j] = size * 2;
+            }
+        }
+
+        //Generate adjacent matrix
+        for(BasicNode node : map){
+            mat[node.getID()][node.getID()] = 1;
+            for( Node node1 : node.getNeighbors()){
+                BasicNode n = (BasicNode) node1;
+                mat[node.getID()][n.getID()] = 1;
+                path[node.getID()][n.getID()] = n;
+            }
+        }
+
+        for (int k = 0; k < size; ++k) {
+            for (int v = 0; v < size; ++v) {
+                for (int w = 0; w < size; ++w) {
+                    if (mat[v][w] > mat[v][k] + mat[k][w]) {
+                        mat[v][w] = mat[v][k] + mat[k][w];
+                        path[v][w] = path[v][k];
+                    }
+                }
+            }
+        }
+
+        int k = path[source][dest].getID();
+        BasicNode kNode = path[source][dest];
+        while(k!=dest){
+            route.offer(kNode);
+            k = path[k][dest].getID();
+            kNode = path[k][dest];
+        }
+        return route;
+    }
+
+    public static boolean isNeighbour(BasicNode source, BasicNode dest){
+        List<Node> neigs = source.getNeighbors();
+        for(Node node : neigs){
+            Integer a = node.getID();
+            Integer b = dest.getID();
+            if(a.equals(b)){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static void send(BasicNode source, BasicNode target, Object obj){
+
+        if( AbstractAgentOperatioin.isNeighbour(source, target)) {
+            MyMessage mm = new MyMessage(obj,null);
+            source.send(target, mm);
+        } else {
+            Queue<BasicNode> path = AbstractAgentOperatioin.findNearestPath(source.getID(), target.getID());
+            path.add(target);
+            logger.info("Sending path:{}", path);
+            MyMessage mm = new MyMessage(obj,path);
+            source.send(path.poll(), mm);
         }
     }
 }
