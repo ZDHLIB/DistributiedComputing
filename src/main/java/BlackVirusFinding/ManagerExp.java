f2dcb43eacf31c99293142d2dd090835de70ba97
DUNHAOZHONG <dunhaozhong@gmail.com>
2017-12-11 15:42:59 -0500

    add terminate and write results to csv

--- src/main/java/BlackVirusFinding/ManagerExp.java
+++ src/main/java/BlackVirusFinding/ManagerExp.java
@@ -1,17 +1,22 @@
 package BlackVirusFinding;
 
 import CommonBean.NodeBean.BasicNode;
+import CommonBean.StatisticInfo;
 import jbotsim.Node;
 import jbotsim.Topology;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import utils.Output;
 
 import java.util.List;
 
 public class ManagerExp {
     private final static Logger logger = LoggerFactory.getLogger(ManagerExp.class);
+    private static StatisticInfo statisticInfo = StatisticInfo.getInstance();
+    private static String filePath = "./src/main/resources/Results/res.csv";
 
-    public static void start(Integer nodeNo){
+    public static void start(Integer nodeNo, String path){
+        filePath = path;
         while( !startRound(nodeNo) ){}
     }
 
@@ -30,6 +35,11 @@ public class ManagerExp {
         }
     }
 
+    public static void writeResult(){
+        String[] res = statisticInfo.getResults();
+        Output.CSVWriteOneLine(res, filePath);
+    }
+
     private static BasicNode getSinkNode(Topology tp){
         List<Node> nodeList = tp.getNodes();
         for(Node node : nodeList){
@@ -38,5 +48,9 @@ public class ManagerExp {
             }
         }
         return null;
+    }
+
+    public static void Terminate(){
+        System.exit(0);
     }
 }
