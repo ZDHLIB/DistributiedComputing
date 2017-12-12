bf7d8ba0eac2851d41fa46f328c051a13143a4b8
DUNHAOZHONG <dunhaozhong@gmail.com>
2017-12-12 12:06:23 -0500

    main

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,15 +1,19 @@
-import BlackVirusFinding.Initiator;
-import CommonBean.NodeBean.BasicNode;
-import jbotsim.Topology;
-import jbotsim.ui.JViewer;
+import BlackVirusFinding.ManagerExp;
+import CommonBean.StatisticInfo;
 
 public class main {
 
-    public static void main(String[] orgs){
+    public static void main(String[] orgs) throws InterruptedException {
+        //Use GUI to start algorithm
+//        Topology topology = Initiator.initTopology(6,600,600,true, BasicNode.class);
+//        new TopologyEvents(topology);
+//        new JViewer(topology);
 
-        Topology topology = Initiator.initTopology(5,600,600,true, BasicNode.class);
+        //Auto running
+        Integer nodeNO = Integer.valueOf(orgs[0]);
+        String filePath = orgs[1];
+        ManagerExp.start(nodeNO, filePath);
 
-        JViewer jViewer = new JViewer(topology);
         return;
     }
 }
