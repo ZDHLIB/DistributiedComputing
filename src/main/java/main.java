e6246aa6038da78321e4dc2689a0728c2d02c81c
DUNHAOZHONG <dunhaozhong@gmail.com>
2017-12-17 22:26:32 -0500

    New version

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,18 +1,25 @@
+import BlackVirusFinding.Initiator;
 import BlackVirusFinding.ManagerExp;
+import CommonBean.NodeBean.BasicNode;
 import CommonBean.StatisticInfo;
+import Events.TopologyEvents;
+import jbotsim.Topology;
+import jbotsim.ui.JViewer;
+
+import java.util.Date;
 
 public class main {
-
+    private static StatisticInfo statisticInfo = StatisticInfo.getInstance();
     public static void main(String[] orgs) throws InterruptedException {
         //Use GUI to start algorithm
-//        Topology topology = Initiator.initTopology(6,600,600,true, BasicNode.class);
-//        new TopologyEvents(topology);
-//        new JViewer(topology);
-
-        //Auto running
         Integer nodeNO = Integer.valueOf(orgs[0]);
         String filePath = orgs[1];
-        ManagerExp.start(nodeNO, filePath);
+        String type = orgs[2];  // "0": greedy based, "1": threshold based
+        statisticInfo.setNoNodes(nodeNO);
+        statisticInfo.setType(type);
+        statisticInfo.setStartTime(new Date().getTime());
+        ManagerExp.setFilePath(filePath);
+        ManagerExp.start(nodeNO, filePath, type);
 
         return;
     }
